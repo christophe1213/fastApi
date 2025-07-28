6fc807288db71ced6b7057e46cc727f30aabe199
chritophe1213 <christophe1213.com@gmail.com>
2025-07-28 15:03:16 +0300

    création fichier html et modification de model

--- app/models/Task.py
+++ app/models/Task.py
@@ -2,8 +2,7 @@ from sqlmodel import SQLModel,Field
 from typing import Optional
 class Task(SQLModel,table= True):
     id:Optional[int]=Field(default=None,primary_key=True)
-    title:str
-    description:str
-    checked:bool    
+    text:str
+    completed:bool    
 
 
--- main.py
+++ main.py
@@ -1,6 +1,23 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from app.routes import tasksRouter
 app = FastAPI()
+# Liste des origines autorisées
+origins = [
+    "null",  #POUR LES FICHIERS HTML LOCAUX
+    # Ajoutez d'autres origines si nécessaire (par exemple, pour un frontend React, Vue, Angular)
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+
+
 @app.get("/")
 def read_root():
     return {"Hello": "World"}
